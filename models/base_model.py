1258ee7ac4da720d265fc974d6cc3f0432045bc8
Crua0316 <cresrugi@gmail.com>
2020-11-02 18:46:53 +0000

    Method constructor

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
     """
 # task 3, 4
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """[summary]
@@ -12,6 +12,28 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """[summary]
         """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
+
+        if kwargs:
+            form = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                    continue
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, form)
+                    continue
+                if key == "update_at":
+                    self.update_at = datetime.strptime(value, form)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """[summary]
