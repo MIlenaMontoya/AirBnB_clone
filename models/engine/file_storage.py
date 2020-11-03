08dd77e86e81abebe740a2a6581e24c2f43ffa6c
Crua0316 <cresrugi@gmail.com>
2020-11-03 21:35:33 +0000

    Fix file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,67 +1,53 @@
-#!/usr/bin/python3
-"""This module holds the class FileStorage
-    that handle all serialization-deserialization,
-    to a JSON file for a persistent model
-    """
+##!/usr/bin/python3
+""" FileStorage Class - serializes and deserializes JSON file to instances """
+
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import models
 
 
 class FileStorage:
-    """Class FileStorage, class tos instance a unique
-    object storage that will process all saving and loading
-    information from the JSON file
-    """
-    __file_path = "file.json"
+    """ This class serializes and deserializes JSON files """
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        """Public Instance Method that return the dictionary
-        with all the current elements on the private field
-        called __objects
-        Returns:
-            [dic]: All the objects, saved from the classes of
-            the project with the structure
-            Class-Name.id(key): obj(value)
-        """
-        return FileStorage.__objects
+        """ Method returns the dictionary __objects """
+        return self.__objects
 
     def new(self, obj):
-        """Public Instance Method that sets the dictionary
-        objects with the information of a new instance
-        Args:
-            obj ([obj]): Objects previusly created from
-            a specify class
-        """
-        FileStorage.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """ sets in __objects the obj with class name and id """
+        new_1 = obj.__class__.__name__
+        new_2 = obj.id
+        new = new_1 + '.' + new_2
+        self.__objects[new] = obj
 
     def save(self):
-        """Public Instance Method that Serialize
-        elements from __objects to a JSON string
-        and save all of those in a JSON file
-        """
-        temp_dict = {}
-        for key, value in FileStorage.__objects.items():
-            temp_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w") as work_file:
-            json.dump(temp_dict, work_file)
+        """ serialization """
+        dict_add = {}
+        with open(self.__file_path, 'w') as f:
+            for key, value in self.__objects.items():
+                dict_add[key] = value.to_dict()
+            json.dump(dict_add, f)
 
     def reload(self):
-        """Public instance Method that Deserializes
-        the JSON strings from the JSON file if it exist
-        and add the elements to the __objects dictionary
-        """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-        from models.state import State
+        """ deserialization """
+        classes = {'BaseModel': BaseModel,
+                   'User': User, 'Place': Place,
+                   'State': State, 'City': City,
+                   'Amenity': Amenity, 'Review': Review}
         try:
-            with open(FileStorage.__file_path, "r") as work_file:
-                data = json.load(work_file)
-                for key, value in data.items():
-                    class_name = key.split(".")
-                    self.__objects[key] = eval(class_name[0])(**value)
-        except Exception:
+            with open(self.__file_path, 'r') as f1:
+                file_store = json.load(f1)
+                for key, value in file_store.items():
+                    if '__class__' in value:
+                        val = classes[value['__class__']](**value)
+                        self.__objects[key] = val
+        except:
             pass
