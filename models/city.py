c7853b5108f21dc9e33401b86de02b02e48381c5
Crua0316 <cresrugi@gmail.com>
2020-11-03 20:36:41 +0000

    Add city class

--- models/city.py
+++ models/city.py
@@ -1,19 +1,10 @@
-
 #!/usr/bin/python3
-<<<<<<< HEAD
 """City Class
-=======
-"""class city
->>>>>>> aca072dcd039a43b89d0543378a6695cd16ca11e
-    """
+"""
+
+
 from models.base_model import BaseModel
 
-<<<<<<< HEAD
-
-class City(BaseModel):
-    """[summary]
-=======
->>>>>>> aca072dcd039a43b89d0543378a6695cd16ca11e
 
 class City(BaseModel):
     """class city from BaseModel
