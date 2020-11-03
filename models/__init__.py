8f2efd3463bd1a80ae672d8165fb341890b9a896
MIlenaMontoya <1852@holbertonschool.com>
2020-11-03 18:29:12 +0000

    comprobando clases

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""This module is the contructor for the
-    models Package and create an instance from
-    FilesStorage and reload all information in
-    the JSON file
-    """
+"""constructor for the
+    models Package """
+    
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """class BaseModel"""
 import uuid
-import datetime
+from datetime import datetime
 from models import storage
 
 class BaseModel:
@@ -11,28 +11,33 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Method constructor initialize an instance
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
-
         if kwargs:
-            form = "%Y-%m-%dT%H:%M:%S.%f"
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            else:
+                self.id = str(uuid.uuid4())
+            if "craeted_at" in kwargs:
+                self.created_at = datetime.datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            else:
+                self.created_at = datetime.datetime.now()
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            else:
+                self.updated_at = datetime.datetime.now()
             for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                    continue
-                if key == "created_at":
-                    self.created_at = datetime.strptime(value, form)
-                    continue
-                if key == "update_at":
-                    self.update_at = datetime.strptime(value, form)
-                    continue
-                if key != "__class__":
+                if key in ["id", "created_at", "updated_at", "__class__"]:
+                    pass
+                else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            models.storage.new(self)
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """representation of an object
