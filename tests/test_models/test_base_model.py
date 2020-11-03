1840df364292a03044cf4c019f32245206fe15ff
MIlenaMontoya <1852@holbertonschool.com>
2020-11-03 11:33:51 +0000

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-"""docstring"""
+"""Test base_model file
+"""
 
 import unittest
+import pep8
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """Tests for the BaseModel class
+    """
+
+    def test_basemodel_pep8(self):
+        """Test PEP8 style
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["./models/base_model.py"])
+        self.assertEqual(result.total_errors, 0)
