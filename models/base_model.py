f2e9bbca911ab00ccb5cc0023d67ef5803d15c33
Crua0316 <cresrugi@gmail.com>
2020-11-03 21:34:59 +0000

    Fix base models

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+"""AirBnb project - BaseModel class"""
 
-"""AirBnB project - Base Models"""
 import uuid
 from datetime import datetime
 import models
@@ -8,15 +8,12 @@ import copy
 
 
 class BaseModel:
-    """New instance BaseModel class
-    """
-
+    """This class defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
-        """Method constructor initialize an instance
-        """
-        self.id = str(uuid4())
+        """Method constructor"""
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs:
             form = "%Y-%m-%dT%H:%M:%S.%f"
@@ -27,8 +24,8 @@ class BaseModel:
                 if key == "created_at":
                     self.created_at = datetime.strptime(value, form)
                     continue
-                if key == "update_at":
-                    self.update_at = datetime.strptime(value, form)
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value, form)
                     continue
                 if key != "__class__":
                     setattr(self, key, value)
@@ -38,27 +35,23 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-
-        """Print a readable sstirng
-        """
+        """Print a readable string"""
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates with the current datetime
-        """
-        self.update_at = datetime.now()
+        """Updates with the current datetime"""
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary with all keys/value
-        of __dict__ of the instance
-        """
+        of __dict__ of the instance"""
         dictn = copy.deepcopy(self.__dict__)
-        dicn['__class__'] = self.__clas__.__name__
+        dictn['__class__'] = self.__class__.__name__
 
         form = "%Y-%m-%dT%H:%M:%S.%f"
         dictn['created_at'] = self.created_at.strftime(form)
-        dictn['updated_at'] = self.update_at.strftime(form)
+        dictn['updated_at'] = self.updated_at.strftime(form)
         dictn['id'] = self.id
         return dictn
